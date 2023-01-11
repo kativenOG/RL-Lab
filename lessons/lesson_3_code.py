f1600af7b580f77c821bb082e590b2ff70afb89b
Andrea Mangrella <mangrella.andrea@gmail.com>
2023-01-11 09:24:29 +0100

    lesson3

--- lessons/lesson_3_code.py
+++ lessons/lesson_3_code.py
@@ -5,26 +5,41 @@ from DangerousGridWorld import GridWorld
 
 
 def on_policy_mc( environment, maxiters=5000, eps=0.3, gamma=0.99 ):
-	"""
-	Performs the on policy version of the every-visit MC control
-	
-	Args:
-		environment: OpenAI Gym environment
-		maxiters: timeout for the iterations
-		eps: random value for the eps-greedy policy (probability of random action)
-		gamma: gamma value, the discount factor for the Bellman equation
-		
-	Returns:
-		policy: 1-d dimensional array of action identifiers where index `i` corresponds to state id `i`
-	"""
+    """
+    Performs the on policy version of the every-visit MC control
 
-	p = [[0 for _ in range(environment.action_space)] for _ in range(environment.observation_space)]   
-	Q = [[0 for _ in range(environment.action_space)] for _ in range(environment.observation_space)]
-	#
-	# YOUR CODE HERE!
-	#
-	deterministic_policy = [numpy.argmax(p[state]) for state in range(environment.observation_space)]	
-	return deterministic_policy
+    Args:
+    environment: OpenAI Gym environment
+    maxiters: timeout for the iterations
+    eps: random value for the eps-greedy policy (probability of random action)
+    gamma: gamma value, the discount factor for the Bellman equation
+		
+    Returns:
+    policy: 1-d dimensional array of action identifiers where index `i` corresponds to state id `i`
+    """
+    p = [[0 for _ in range(environment.action_space)] for _ in range(environment.observation_space)]   # policy, manca epsilon soft initialization, scegli un azioene a caso per ogni stato e li metti il valore di (1-epsilon)/(epsilon- #_azioni)
+    Q = [[0 for _ in range(environment.action_space)] for _ in range(environment.observation_space)]
+    returns = [[[] for _ in range(environment.action_space)] for _ in range(environment.observation_space)]
+    
+    for _ in range(maxiters):
+
+        episode = environment.sample_episode(p)[::-1]
+        G = 0 
+        for val in episode:
+            stato,azione,reward = val[0],val[1],val[2]
+            G = gamma*G + reward 
+            returns[stato][azione].append(G)    
+            Q[stato][azione] = int(sum(G)/len(G))
+
+            actions = []
+            for action in range(environment.action_space): actions.append([Q[azione][action], action]) 
+            best_action = max(actions)[1]
+            
+            for action in range(environment.action_space):
+                p[stato][action] = (1- eps + (eps/len(environment.action))) if (action == best_action) else (eps/len(environment.action))
+
+    deterministic_policy = [numpy.argmax(p[state]) for state in range(environment.observation_space)]	
+    return deterministic_policy
 
 
 def main():
