67735a6394363ace9fc1dcf2b211b69cd1a1889b
Andrea Mangrella <mangrella.andrea@gmail.com>
2023-03-27 13:49:34 +0200

    Lab 8

--- lessons/lesson_8_code.py
+++ lessons/lesson_8_code.py
@@ -12,140 +12,153 @@ np.random.seed(SEED); tf.random.set_seed(SEED)
 
 
 def createDNN( nInputs, nOutputs, nLayer, nNodes ):
-	"""
-	Function that generates a neural network with the given requirements.
-
-	Args:
-		nInputs: number of input nodes
-		nOutputs: number of output nodes
-		nLayer: number of hidden layers
-		nNodes: number nodes in the hidden layers
-		
-	Returns:
-		model: the generated tensorflow model
-
-	"""
-	
-	# Initialize the neural network
-	model = Sequential()
-	#
-	# YOUR CODE HERE!
-	#
-	return model
+    """
+    Function that generates a neural network with the given requirements.
+    
+    Args:
+    	nInputs: number of input nodes
+    	nOutputs: number of output nodes
+    	nLayer: number of hidden layers
+    	nNodes: number nodes in the hidden layers
+    	
+    Returns:
+    	model: the generated tensorflow model
+    
+    """
+    
+    # Initialize the neural network
+    model = Sequential()
+    model.add(Dense(nNodes, input_dim=nInputs, activation="relu")) #input layer + hidden layer #1
+    for _ in range(1,nLayer):
+        model.add(Dense(nNodes, activation="relu")) #hidden layer #2
+    model.add(Dense(nOutputs, activation="linear")) #output layer
+    
+    return model
 
 
 def mse( network, dataset_input, target ):
-	"""
-	Compute the MSE loss function
-
-	"""
-	
-	# Compute the predicted value, over time this value should
-	# looks more like to the expected output (i.e., target)
-	predicted_value = network( dataset_input )
-	
-	# Compute MSE between the predicted value and the expected labels
-	mse = tf.math.square(predicted_value - target)
-	mse = tf.math.reduce_mean(mse)
-	
-	# Return the averaged values for computational optimization
-	return mse
+    """
+    Compute the MSE loss function
+    
+    """
+    # Compute the predicted value, over time this value should
+    # looks more like to the expected output (i.e., target)
+    predicted_value = network( dataset_input )
+    
+    # Compute MSE between the predicted value and the expected labels
+    mse = tf.math.square(predicted_value - target)
+    mse = tf.math.reduce_mean(mse)
+    
+    # Return the averaged values for computational optimization
+    return mse
 
 
 def training_loop( env, neural_net, updateRule, eps=1, episodes=100, updates=1 ):
-	"""
-	Main loop of the reinforcement learning algorithm. Execute the actions and interact
-	with the environment to collect the experience for the training.
+    """
+    Main loop of the reinforcement learning algorithm. Execute the actions and interact
+    with the environment to collect the experience for the training.
+    
+    Args:
+    	env: gymnasium environment for the training
+    	neural_net: the model to train 
+    	updateRule: external function for the training of the neural network
+    	
+    Returns:
+    	averaged_rewards: array with the averaged rewards obtained
+    
+    """
+    
+    # Initialize the optimizer 
+    # Not Stochastic Gradient Descent but Adam
+    optimizer = tf.keras.optimizers.Adam( learning_rate=0.001 ) 
+    rewards_list, memory_buffer = [], collections.deque( maxlen=1000 )
+    averaged_rewards = []
+    for ep in range(episodes):
+    
+        # Reset the environment and obtain the initial state
+        state = env.reset()[0]
+        state = state.reshape(-1,4)
+        # print(f"STATE: {state}")
+        # print(f"NEW STATE: {state}")
+        ep_reward = 0
+        while True:
+        
+            #TODO: select the action to perform
+            action = env.action_space.sample() 
+            
+            # Perform the action, store the data in the memory buffer and update the reward
+            next_state, reward, terminated, truncated, info = env.step(action)
+            next_state = state.reshape(-1,4)
+            memory_buffer.append( list([state,action,reward,next_state,terminated])) 
+            ep_reward += reward
+            
+            # Perform the actual training
+            updateRule( neural_net, memory_buffer, optimizer )
+            
+            #TODO: exit condition for the episode
+            if terminated or truncated: 
+                break
+            
+            # Update the current state
+            state = next_state 
+        
+        # Update the reward list to return
+        rewards_list.append( ep_reward )
+        averaged_rewards.append( np.mean(rewards_list) )
+        print( f"episode {ep:2d}: rw: {averaged_rewards[-1]:3.2f}, eps: {eps:3.2f}" )
 
-	Args:
-		env: gymnasium environment for the training
-		neural_net: the model to train 
-		updateRule: external function for the training of the neural network
-		
-	Returns:
-		averaged_rewards: array with the averaged rewards obtained
-
-	"""
-
-	#TODO: initialize the optimizer 
-	optimizer = None 
-	rewards_list, memory_buffer = [], collections.deque( maxlen=1000 )
-	averaged_rewards = []
-	for ep in range(episodes):
-
-		#TODO: reset the environment and obtain the initial state
-		state = None 
-		ep_reward = 0
-		while True:
-
-			#TODO: select the action to perform
-			action = None 
-
-			#TODO: Perform the action, store the data in the memory buffer and update the reward
-			memory_buffer.append( None )
-			ep_reward += None
-
-			# Perform the actual training
-			updateRule( neural_net, memory_buffer, optimizer )
-
-			#TODO: exit condition for the episode
-			if False: break
-
-			#TODO: update the current state
-			state = None
-
-		# Update the reward list to return
-		rewards_list.append( ep_reward )
-		averaged_rewards.append( np.mean(rewards_list) )
-		print( f"episode {ep:2d}: rw: {averaged_rewards[-1]:3.2f}, eps: {eps:3.2f}" )
-
-	# Close the enviornment and return the rewards list
-	env.close()
-	return averaged_rewards
+    # Close the enviornment and return the rewards list
+    env.close()
+    return averaged_rewards
 
 
 def DQNUpdate( neural_net, memory_buffer, optimizer, batch_size=32, gamma=0.99 ):
-
-	"""
-	Main update rule for the DQN process. Extract data from the memory buffer and update 
-	the newtwork computing the gradient.
-
-	"""
-
-	if len(memory_buffer) < batch_size: return
-
-	indices = np.random.randint( len(memory_buffer), size=batch_size)
-	for idx in indices: 
-
-		#TODO: extract data from the buffer 
-		state, action, reward, next_state, done = None, None, None, None, None
-
-		#TODO: compute the target for the training
-		target = None
-
-		#TODO: compute the gradient and perform the backpropagation step
-		with tf.GradientTape() as tape:
-			objective = mse( neural_net, state, target )
+    
+    """
+    Main update rule for the DQN process. Extract data from the memory buffer and update 
+    the newtwork computing the gradient.
+    
+    """
+    # Check if we have enough elements in the replay buffer (memory_buffer)
+    if len(memory_buffer) < batch_size: return
+    # Generate #n_batch_size random indeces
+    indices = np.random.randint( len(memory_buffer), size=batch_size) 
+    for idx in indices: 
+        # Extract data from the buffer 
+        state, action, reward, next_state, done = memory_buffer[idx]
+        
+        #TODO: compute the target for the training
+        if done:
+            target = reward 
+        else:
+            max_q = neural_net(next_state)  
+            target = reward + (max_q*gamma) 
+        
+    # Compute the gradient and perform the backpropagation step
+    with tf.GradientTape() as tape:
+        objective = mse( neural_net, state, target )
+        grad = tape.gradient(objective, neural_net.trainable_variables )
+        optimizer.apply_gradients( zip(grad, neural_net.trainable_variables) )
 
 
 def main():
-	print( "\n************************************************" )
-	print( "*  Welcome to the eighth lesson of the RL-Lab!   *" )
-	print( "*               (Deep Q-Network)                 *" )
-	print( "**************************************************\n" )
-
-	_training_steps = 100
-
-	env = gymnasium.make( "CartPole-v1" )#, render_mode="human" )
-	neural_net = createDNN( 4, 2, nLayer=2, nNodes=32)
-	rewards = training_loop( env, neural_net, DQNUpdate, episodes=_training_steps  )
-
-	t = np.arange(0, _training_steps)
-	plt.plot(t, rewards, label="eps: 0", linewidth=3)
-	plt.xlabel( "episodes", fontsize=16)
-	plt.ylabel( "reward", fontsize=16)
-	plt.show()
+    print( "\n************************************************" )
+    print( "*  Welcome to the eighth lesson of the RL-Lab!   *" )
+    print( "*               (Deep Q-Network)                 *" )
+    print( "**************************************************\n" )
+    
+    _training_steps = 100
+    
+    env = gymnasium.make( "CartPole-v1" )#, render_mode="human" )
+    neural_net = createDNN( 4, 2, nLayer=2, nNodes=32)
+    rewards = training_loop( env, neural_net, DQNUpdate, episodes=_training_steps  )
+    
+    t = np.arange(0, _training_steps)
+    plt.plot(t, rewards, label="eps: 0", linewidth=3)
+    plt.xlabel( "episodes", fontsize=16)
+    plt.ylabel( "reward", fontsize=16)
+    plt.show()
 
 
 if __name__ == "__main__":
-	main()	
+    main()	
