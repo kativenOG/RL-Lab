cd6d8cc970d31c106a3790d6f59b1d20f787fc34
Andrea Mangrella <mangrella.andrea@gmail.com>
2023-05-10 12:35:09 +0200

    fixed a2c

--- lessons/lesson_10_code.py
+++ lessons/lesson_10_code.py
@@ -37,37 +37,28 @@ def training_loop( env, actor_net, critic_net, updateRule, frequency=10, episode
     critic_optimizer = tf.keras.optimizers.Adam( learning_rate=0.001 ) 
     rewards_list, reward_queue = [], collections.deque( maxlen=100 )
     memory_buffer = []
-    critic_memory_buffer= []
     for ep in range(episodes):
     
-        partial_memory_buffer = []
         state = env.reset()[0] 
         state = state.reshape(-1,4)
         ep_reward = 0
         while True:
         
-            # action = env.action_space.sample() 
             # La prossim azioend eve essere scelta il base alla policy predetta dall'actor
-            n_action = 2 
             distribution = actor_net(state).numpy()[0]
             action = np.random.choice(env.action_space.n,p=distribution)
             
-            next_state, reward, terminated, truncated, info = env.step(action)
+            next_state, reward, terminated, truncated, _ = env.step(action)
             next_state = next_state.reshape(-1,4)
-            
-            partial_memory_buffer.append( list([state,action,reward,next_state,terminated])) 
-            critic_memory_buffer.append( list([state,action,reward,next_state,terminated])) 
+            memory_buffer.append([state,action,reward,next_state,terminated])
             ep_reward += reward
-            
             if terminated or truncated:  break
             state = next_state
         
-        # Perform the actual training every 'frequency' episodes
-        memory_buffer.append(partial_memory_buffer)
-        partial_memory_buffer = []
+        # Perform the training every 'frequency'(10) episodes
+        # memory_buffer.append(partial_memory_buffer)
         if ep %frequency == 0 and ep!=0: 
-            updateRule( actor_net,critic_net, memory_buffer, critic_memory_buffer, actor_optimizer, critic_optimizer)
-            critic_memory_buffer = []
+            updateRule( actor_net, critic_net, memory_buffer, actor_optimizer, critic_optimizer) # critic_memory_buffer
             memory_buffer = []
     
         # Update the reward list to return
@@ -81,7 +72,7 @@ def training_loop( env, actor_net, critic_net, updateRule, frequency=10, episode
 
 
 
-def A2C( actor_net, critic_net, memory_buffer,critic_memory_buffer, actor_optimizer, critic_optimizer, gamma=0.99 ):
+def A2C( actor_net, critic_net, memory_buffer, actor_optimizer, critic_optimizer, gamma=0.99 ):
 
     """
     ###Notes###
@@ -91,54 +82,35 @@ def A2C( actor_net, critic_net, memory_buffer,critic_memory_buffer, actor_optimi
 	and for the critic network (or value function)
 
 	"""
-    # critic_memory_buffer = np.array(critic_memory_buffer)
-    # memory_buffer = np.array(memory_buffer)
     # UPDATE RULE CRITIC 
+    instance = numpy.array(memory_buffer)
     for _ in range(10):
         # Shuffle the memory buffer
-        np.random.shuffle( critic_memory_buffer )
-        #TODO: Compute the target and the MSE between the current prediction and the expected advantage 
-        for instance in critic_memory_buffer:
-            state, action, reward, next_state, done = instance  
-            done = 1 if done else 0
-            target = reward + (1 - done)*gamma*critic_net(next_state).numpy()[0][0] #done.astype(int)
-            # CRITIC TAPE 
-            with tf.GradientTape() as critic_tape:
-                #MSE:
-                # objective = mse(critic_net,state,target)
-                predicted = critic_net(state)
-                objective= tf.math.square(predicted - target)
-                grad = critic_tape.gradient(objective, critic_net.trainable_variables )
-                critic_optimizer.apply_gradients( zip(grad, critic_net.trainable_variables) )
+        np.random.shuffle( instance)
+        state,action,reward,next_state,dones = np.vstack(instance[:,0]),np.vstack(instance[:,1]),np.vstack(instance[:,2]),np.vstack(instance[:,3]),np.vstack(instance[:,4])
+        target = reward + (1 - dones.astype(int))*gamma*critic_net(next_state).numpy()[0][0]
+        # CRITIC TAPE 
+        with tf.GradientTape() as critic_tape:
+            predicted = critic_net(state)
+            objective= tf.math.square(predicted - target)
+            grad = critic_tape.gradient(objective, critic_net.trainable_variables)
+            critic_optimizer.apply_gradients( zip(grad, critic_net.trainable_variables) )
 
     # ACTOR TAPE 
-    #TODO: compute the log-prob of the current trajectory and 
     with tf.GradientTape() as actor_tape:
-        objectives = []
-
-        for instance in memory_buffer:
-            instance = numpy.array(instance)
-            state =      instance[:,0]
-            action =     instance[:,1].tolist()
-            reward =     instance[:,2].tolist()
-            next_state = instance[:,3]
-
-            # The objective function, notice that:
-            # The REINFORCE objective is the sum of the logprob (i.e., the probability of the trajectory) multiplied by advantage
-            objectives = []
-            target = 0 
-            for i in range(len(state)):
-                appo = actor_net(state[i])
-                log_probs = tf.math.log(appo[0][action[i]])
-                adv_a = reward[i] + gamma * critic_net(next_state[i]).numpy().reshape(-1)
-                adv_b = critic_net(state[i]).numpy().reshape(-1)
-                target += log_probs * (adv_a[0] - adv_b[0])
-            objectives.append(target)
-                
+        objective = 0 # []
+        for row in instance:
+            state,action,reward,next_state,dones = row 
+            appo = actor_net(state)
+            log_probs = tf.math.log(appo[0][action])
+            adv_a = reward + gamma * critic_net(next_state).numpy().reshape(-1)
+            adv_b = critic_net(state).numpy().reshape(-1)
+            # objective.append(log_probs * (adv_a[0] - adv_b[0]))
+            objective += log_probs * (adv_a[0] - adv_b[0])
                     
         # Computing the final objective to optimize, is the average between all the considered trajectories
-        objective = -tf.math.reduce_mean(objectives)
-        grad = actor_tape.gradient(objective,actor_net.trainable_variables)
+        # to_optimize = -tf.math.reduce_mean(objective) # inutile 
+        grad = actor_tape.gradient(-objective,actor_net.trainable_variables)
         actor_optimizer.apply_gradients( zip(grad, actor_net.trainable_variables) )
 
 	
--- lessons/lesson_11_code.py
+++ lessons/lesson_11_code.py
@@ -8,26 +8,131 @@ import gymnasium, collections
 
 
 # TODO: implement the following functions as in the previous lessons
-def createDNN( nInputs, nOutputs, nLayer, nNodes, last_activation ): raise NotImplementedError
-def training_loop( env, actor_net, critic_net, updateRule, frequency=10, episodes=100 ): raise NotImplementedError
-def A2C( actor_net, critic_net, memory_buffer, actor_optimizer, critic_optimizer, gamma=0.99, observation_number=None ): raise NotImplementedError
+def createDNN( nInputs, nOutputs, nLayer, nNodes, last_activation ): 
+    # Initialize the neural network
+    model = Sequential()
+    model.add(Dense(nNodes, input_dim=nInputs, activation="relu")) #input layer + hidden layer #1
+    for _ in range(1,nLayer):
+        model.add(Dense(nNodes, activation="relu")) #hidden layer 
+    model.add(Dense(nOutputs, activation=last_activation)) #output layer
+    return model
+
+def training_loop( env, actor_net, critic_net, updateRule, frequency=10, episodes=100 ): 
+    actor_optimizer = tf.keras.optimizers.Adam( learning_rate=0.001 ) 
+    critic_optimizer = tf.keras.optimizers.Adam( learning_rate=0.001 ) 
+    rewards_list, reward_queue = [], collections.deque( maxlen=100 )
+    memory_buffer = []
+    critic_memory_buffer = []
+    for ep in range(episodes):
+    
+        partial_memory_buffer = []
+        state = env.reset()[0] 
+        state = state.reshape(-1,2)
+        ep_reward = 0
+        while True:
+        
+            # action = env.action_space.sample() 
+            # La prossim azioend eve essere scelta il base alla policy predetta dall'actor
+            n_action = 3 
+            distribution = actor_net(state).numpy()[0]
+            action = np.random.choice(env.action_space.n,p=distribution)
+            
+            next_state, reward, terminated, truncated, info = env.step(action)
+            next_state = next_state.reshape(-1,2)
+            
+            partial_memory_buffer.append( [state,action,reward,next_state,terminated])
+            critic_memory_buffer.append( list([state,action,reward,next_state,terminated])) 
+            ep_reward += reward
+            
+            if terminated or truncated:  break
+            state = next_state
+        
+        # Perform the actual training every 'frequency' episodes
+        memory_buffer.append(partial_memory_buffer)
+        partial_memory_buffer = []
+        if ep %frequency == 0 and ep!=0: 
+            updateRule( actor_net,critic_net,critic_memory_buffer, memory_buffer, actor_optimizer, critic_optimizer) # critic_memory_buffer
+            critic_memory_buffer = []
+            memory_buffer = []
+    
+        # Update the reward list to return
+        reward_queue.append( ep_reward )
+        rewards_list.append( np.mean(reward_queue) )
+        print( f"episode {ep:4d}: rw: {int(ep_reward):3d} (averaged: {np.mean(reward_queue):5.2f})" )
+    
+    #Close the enviornment and return the rewards list
+    env.close()
+    return rewards_list
+
+def A2C( actor_net, critic_net,critic_memory_buffer, memory_buffer, actor_optimizer, critic_optimizer, gamma=0.99, observation_number=None ): 
+    # UPDATE RULE CRITIC 
+    for _ in range(10):
+        # Shuffle the memory buffer
+        np.random.shuffle( critic_memory_buffer )
+        #TODO: Compute the target and the MSE between the current prediction and the expected advantage 
+        for instance in critic_memory_buffer:
+            state, action, reward, next_state, done = instance  
+            done = 1 if done else 0
+            target = reward + (1 - done)*gamma*critic_net(next_state).numpy()[0][0] 
+            # CRITIC TAPE 
+            with tf.GradientTape() as critic_tape:
+                #MSE:
+                predicted = critic_net(state)
+                objective= tf.math.square(predicted - target)
+                grad = critic_tape.gradient(objective, critic_net.trainable_variables)
+                critic_optimizer.apply_gradients( zip(grad, critic_net.trainable_variables) )
+
+    # ACTOR TAPE 
+    with tf.GradientTape() as actor_tape:
+        objectives = []
+
+        for instance in memory_buffer:
+            instance = np.array(instance)
+            state,action,reward,next_state,dones = instance[:,0],instance[:,1],instance[:,2],instance[:,3],instance[:,4]
+            # The objective function, notice that:
+            # The REINFORCE objective is the sum of the logprob (i.e., the probability of the trajectory) multiplied by advantage
+            objectives = []
+            target = 0 
+            for i in range(len(state)):
+                appo = actor_net(state[i])
+                log_probs = tf.math.log(appo[0][action[i]])
+                adv_a = reward[i] + gamma * critic_net(next_state[i]).numpy().reshape(-1)
+                adv_b = critic_net(state[i]).numpy().reshape(-1)
+                target += log_probs * (adv_a[0] - adv_b[0])
+            objectives.append(target)
+                
+        # Computing the final objective to optimize, is the average between all the considered trajectories
+        objective = -tf.math.reduce_mean(objectives)
+        grad = actor_tape.gradient(objective,actor_net.trainable_variables)
+        actor_optimizer.apply_gradients( zip(grad, actor_net.trainable_variables) )
 
 
 # TODO: implement the following class
 class OverrideReward( gymnasium.wrappers.NormalizeReward ):
-	"""
-	Gymansium wrapper useful to update the reward function of the environment
+    """
+    Gymansium wrapper useful to update the reward function of the environment
+    """
+    def step(self, action):
+        previous_observation = np.array(self.env.state, dtype=np.float32)
+        observation, reward, terminated, truncated, info = self.env.step(action)
+        # Prof Method 1:
+        if observation[1]==1: reward = -10  
+        elif terminated: reward = 1000  
+        elif(previous_observation[0] - observation[0])>0 and observation[1] == 0 : reward = 1 # Sto andando verso sinistra e spingendo verso sinistra 
+        elif(previous_observation[0] - observation[0])<0 and observation[1] == 2 : reward = 1  # Sto andando verso destra e spingendo verso destra 
+        return observation, reward, terminated, truncated, info
 
-	"""
+        # actual_reward = 0 
+        # epsilon = 1e03
+        # if terminated: actual_reward = 10000000
+        # elif (action == 1): actual_reward = -1000000
+        # elif (action==0):
+        #     if sum(observation) <= -1.6: actual_reward = -10000
+        #     actual_reward = -1/(1.6+observation[0]+ epsilon) -observation[1]*100
+        # elif (action==2):
+        #     if sum(observation) >= 0.5: actual_reward = 1000000
+        #     actual_reward = -1/(0.5-observation[0]+ epsilon) + observation[1]*100
 
-	def step(self, action):
-		previous_observation = np.array(self.env.state, dtype=np.float32)
-		observation, reward, terminated, truncated, info = self.env.step( action )
-		
-		#TODO: extract the information from the observations
-		#TODO: override the reward function before the return
-
-		return observation, reward, terminated, truncated, info
 	
 
 def main(): 
